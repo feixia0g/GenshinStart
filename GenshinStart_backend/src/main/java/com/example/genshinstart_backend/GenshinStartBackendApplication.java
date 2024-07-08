c53004cccdab71d3289e2ff60ea95e0d88eb718a
feixia0g <591337215@qq.com>
2024-07-08 15:26:54 +0800

    chore:初始化后端项目目录结构以及github action的自动化ci测试 * 初始化后端项目目录结构 * 初始化github action的自动化ci测试

---
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/GenshinStartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.genshinstart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinStartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenshinStartBackendApplication.class, args);
+    }
+
+}
