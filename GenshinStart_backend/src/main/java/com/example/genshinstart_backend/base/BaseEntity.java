52668c099080e11284dd5bb02609a0928b0a4fbc
feixia0g <591337215@qq.com>
2024-07-09 19:50:09 +0800

    feat:添加BaseEntity抽象类 * 添加BaseEntity抽象类

---
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/base/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.example.genshinstart_backend.base;
+
+/**
+ * @version 1.0.0
+ * @description: The type Base entity.
+ * @author feixia0g
+ * @date 2024/7/9 19:50
+ */
+public abstract class BaseEntity {
+
+}
