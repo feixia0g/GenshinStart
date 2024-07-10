502849b1dbdcd6c5a75d8da59c61c81a49a59e06
feixia0g <591337215@qq.com>
2024-07-10 20:49:06 +0800

    feat:实现QuestionController各个功能接口 * 添加Question类 * 添加QuestionMapper和QuestionMapper.xml * 添加QuestionService和实现QuestionServiceImpl * 添加QuestionController的传输类：QuestionEditItemVM/QuestionEditRequestVM/QuestionPageRequest * 添加QuestionStatusEnum/QuestionTypeEnum枚举类 * 添加QuestionItemObject/QuestionObject * 添加ExamUtil工具类

---
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/repository/QuestionMapper.java
@@ -0,0 +1,22 @@
+package com.example.genshinstart_backend.repository;
+
+import com.example.genshinstart_backend.domain.Question;
+import com.example.genshinstart_backend.domain.other.KeyValue;
+import com.example.genshinstart_backend.viewmodel.admin.question.QuestionPageRequestVM;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
+@Mapper
+public interface QuestionMapper extends BaseMapper<Question> {
+
+    List<Question> page(QuestionPageRequestVM requestVM);
+
+    List<Question> selectByIds(@Param("ids") List<Integer> ids);
+
+    Integer selectAllCount();
+
+    List<KeyValue> selectCountByDate(@Param("startTime") Date startTime,@Param("endTime") Date endTime);
+}
