2cef8a9a2539426aa72fabb080c123c3bbfaca47
Algernon-qaq <321801487@qq.com>
2024-07-10 10:09:14 +0800

    feat: 新增 layout 组件代码,美化布局效果 #2

--- GenshinStart_frontend/vue/student/src/main.js
+++ GenshinStart_frontend/vue/student/src/main.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line semi
-import Vue from 'vue';
+import Vue from 'vue'
 // eslint-disable-next-line semi
-import App from './App.vue';
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   // eslint-disable-next-line comma-dangle
-  render: (h) => h(App),
+  render: (h) => h(App)
   // eslint-disable-next-line semi
-}).$mount('#app');
+}).$mount('#app')
