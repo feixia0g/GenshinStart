a8e4bb4ee5911c1a46fa0feb96a59b20b339f3d7
feixia0g <591337215@qq.com>
2024-07-10 21:12:06 +0800

    feat:添加ExamPaperService和实现ExamPaperServiceImpl * ExamPaperService和实现ExamPaperServiceImpl * 添加ExamPaperEditRequestVM/ExamPaperQuestionItemObject/ExamPaperTitleItemObject/ExamPaperTitleItemVM * 添加ExamPaperTypeEnum枚举类

--- GenshinStart_backend/src/main/java/com/example/genshinstart_backend/utility/ExamUtil.java
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/utility/ExamUtil.java
@@ -8,8 +8,8 @@ import java.util.stream.Collectors;
 /**
  * @version 3.5.0
  * @description: The type Exam util.
- * Copyright (C), 2020-2024, 武汉思维跳跃科技有限公司
- * @date 2021/12/25 9:45
+ * @author feixia0g
+ * @date 2024/7/10 21:00
  */
 public class ExamUtil {
 
