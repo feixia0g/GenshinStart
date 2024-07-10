f1489e279b415df336598d4aa2bff7de9f819d0d
Algernon-qaq <321801487@qq.com>
2024-07-10 10:40:30 +0800

    feat: 封装api代码,优化代码结构 #2

--- GenshinStart_frontend/vue/student/src/main.js
+++ GenshinStart_frontend/vue/student/src/main.js
@@ -1,12 +1,51 @@
-// eslint-disable-next-line semi
 import Vue from 'vue'
-// eslint-disable-next-line semi
 import App from './App.vue'
+import { router } from './router'
+import store from './store'
+import 'normalize.css/normalize.css'
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+
+import '@/styles/index.scss' // global css
+import './icons' // icon
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+
+Vue.use(Element, {
+  size: 'medium' // set element-ui default size
+})
 
 Vue.config.productionTip = false
 
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+router.beforeEach(async (to, from, next) => {
+  // start progress bar
+  NProgress.start()
+  if (to.meta.title !== undefined) {
+    document.title = to.meta.title
+  } else {
+    document.title = '\u200E'
+  }
+  store.commit('router/initRoutes')
+
+  if (to.path) {
+    // eslint-disable-next-line no-undef
+    _hmt.push(['_trackPageview', '/#' + to.fullPath])
+  }
+
+  next()
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
+
+Vue.prototype.$$router = router
+
 new Vue({
-  // eslint-disable-next-line comma-dangle
+  router: router,
+  store: store,
   render: (h) => h(App)
-  // eslint-disable-next-line semi
 }).$mount('#app')
