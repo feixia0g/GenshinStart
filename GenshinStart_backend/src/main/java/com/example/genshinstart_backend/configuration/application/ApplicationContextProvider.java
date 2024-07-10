7373955435a09cf3a0846c37241a90f057197f58
feixia0g <591337215@qq.com>
2024-07-10 08:57:33 +0800

    feat:添加ApplicationContextProvider类 * 添加ApplicationContextProvider类 * 用于获取Spring中管理的Bean类

---
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/configuration/application/ApplicationContextProvider.java
@@ -0,0 +1,50 @@
+package com.example.genshinstart_backend.configuration.application;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @version 1.0.0
+ * @description: The type Application context provider.
+ * @author feixia0g
+ * @date 2024/7/10 8:55
+ */
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    private ApplicationContextProvider() {
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    /**
+     * Gets bean.
+     *
+     * @param <T>    the type parameter
+     * @param aClass the a class
+     * @return the bean
+     */
+    public static <T> T getBean(Class<T> aClass) {
+        return context.getBean(aClass);
+    }
+
+    /**
+     * Gets bean.
+     *
+     * @param <T>  the type parameter
+     * @param name the name
+     * @return the bean
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        return (T) context.getBean(name);
+    }
+}
+
+
