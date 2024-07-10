56d815938895abd244d9d98b1f76fe98ad78a222
feixia0g <591337215@qq.com>
2024-07-10 09:41:46 +0800

    feat:添加LoginAuthenticationEntryPoint类 * 添加LoginAuthenticationEntryPoint类 * LoginAuthenticationEntryPoint类在用户未登录会重定向到/api/user/login

---
+++ GenshinStart_backend/src/main/java/com/example/genshinstart_backend/configuration/spring/security/LoginAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package com.example.genshinstart_backend.configuration.spring.security;
+
+import com.example.genshinstart_backend.base.SystemCode;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @version 1.0.0
+ * @description: 未登录
+ * @author feixia0g
+ * @date 2024/7/10 10:00
+ */
+@Component
+public final class LoginAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
+
+    /**
+     * Instantiates a new Login authentication entry point.
+     */
+    public LoginAuthenticationEntryPoint() {
+        super("/api/user/login");
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        RestUtil.response(response, SystemCode.UNAUTHORIZED);
+    }
+
+}
+
